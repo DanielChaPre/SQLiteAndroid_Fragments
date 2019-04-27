4cf192c08b8154c466897f0335b02ab28c9abd74
DanielChaPre <danchavez197@gmail.com>
2019-04-27 13:57:33 -0500

    cambios

--- App1/App1/MainActivity.cs
+++ App1/App1/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.Widget;
 namespace App1
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
